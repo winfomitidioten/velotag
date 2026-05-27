8b2ade95b0b7ceea762784f0260a692da3971c66
thorbenhellweger <thorben.hellweger2@icloud.com>
2026-05-27 20:31:17 +0200

    Strava Guideline Button

--- frontend/src/components/GpxUploadModal.vue
+++ frontend/src/components/GpxUploadModal.vue
@@ -15,7 +15,9 @@
       <h2>Fahrt hochladen</h2>
       <p>.gpx Datei auswählen</p>
       <div> 
-        <button @click="connectStrava" class="btn_strava_api">Mit Strava verbinden</button>
+        <a @click="connectStrava"> 
+          <img src="@/assets/btn_strava_connect_with_orange.png" alt="Connect with Strava" />
+        </a>
       </div>
       <div>
         <button class="btn_upload_manual">Datei auswählen</button>
