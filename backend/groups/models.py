f8c1907aa5fc10f199e4e5c036193f37fa2885f6
moritz <moritz.rak@gmail.com>
2026-06-11 17:33:44 +0200

    memberhsip model und post methode angepasst

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import UniqueConstraint 
 
 # Create your models here.
 class Group(models.Model):
     name = models.CharField(max_length=150, blank=False)
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='administered_groups')
-    members = models.ManyToManyField(User, related_name='group_memberships')
+    members = models.ManyToManyField(User, related_name='group_memberships', through='Membership')
 
     def __str__(self):
         return self.name
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    status = models.CharField(max_length=150, blank=False, default='Pending')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'group'],
+                name='unique_user_group_membership'
+            )
+        ]
